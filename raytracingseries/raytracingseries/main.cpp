c9683873d9d355664ebf79e9e6feaf57f9e1f012
andy <acbeers1@gmail.com>
2023-07-08 02:10:54 -0400

    feat: 2.3 progress indicator

--- raytracingseries/raytracingseries/main.cpp
+++ raytracingseries/raytracingseries/main.cpp
@@ -23,6 +23,7 @@ int main(int argc, const char * argv[]) {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     
     for (int j = image_height - 1; j >= 0; --j){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -35,6 +36,7 @@ int main(int argc, const char * argv[]) {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "\nDone.\n";
     
     std::cout.rdbuf(coutbuf);
 }
